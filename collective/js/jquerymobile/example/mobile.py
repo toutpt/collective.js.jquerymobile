64e1695773447885cb048b02dd6e6fc1d13f120f
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-04 14:07:31 +0100

    make example works

--- collective/js/jquerymobile/example/mobile.py
+++ collective/js/jquerymobile/example/mobile.py
@@ -1,5 +1,12 @@
 from Products.Five import BrowserView
 
+
 class Example(BrowserView):
     """Example browserview"""
-    
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def pageid(self):
+        return '-'.join(self.context.getPhysicalPath())[1:]
